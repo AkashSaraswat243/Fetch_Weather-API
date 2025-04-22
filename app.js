d4d4474278db02037b67e81ec8729c0c4355720e
Akash Saraswat <AkashSaraswat243@gmail.com>
2025-04-22 19:52:31 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+async function getWeather() {
+    const city = document.getElementById("cityInput").value;
+    const resultDiv = document.getElementById("weatherResult");
+
+    if (!city) {
+        resultDiv.innerText = "Please enter a city name.";
+        return;
+    }
+
+    const apiKey = "c51d26aa60f745909e6140444252204";
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("City not found");
+
+        const data = await response.json();
+        const temperature = data.current.temp_c;
+        const condition = data.current.condition.text;
+        resultDiv.innerHTML = `
+      <strong>${data.location.name}</strong><br>
+      Temperature: ${temperature}°C<br>
+      Condition: ${condition}
+    `;
+    } catch (error) {
+        resultDiv.innerText =
+            "Error fetching weather data. Please try again.";
+    }
+}
